acbc80623e0f3f2d5194aee91d2205b004b420f0
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2024-09-06 21:28:26 +0530

    firstone

---
+++ css/password.js
@@ -0,0 +1,17 @@
+
+"use strict"
+
+// for show password 
+let createpassword = (type, ele) => {
+    document.getElementById(type).type = document.getElementById(type).type == "password" ? "text" : "password";
+    let icon = ele.childNodes[0].classList;
+    let stringIcon = icon.toString()
+    if (stringIcon.includes("fa-eye")) {
+        ele.childNodes[0].classList.remove("fa-eye");
+        ele.childNodes[0].classList.add("fa-eye-slash");
+    }
+    else {
+        ele.childNodes[0].classList.add("fa-eye");
+        ele.childNodes[0].classList.remove("fa-eye-slash");
+    }
+}
